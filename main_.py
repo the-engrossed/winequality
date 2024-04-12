04eca184851f0514f3f6f665d1653fc1c71632a0
the-engrossed <tanmaireddy5598@gmail.com>
2024-04-12 12:36:55 -0400

    datavalidation

--- main_.py
+++ main_.py
@@ -1,5 +1,6 @@
 from winepred import logger
 from winepred.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from winepred.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -12,3 +13,12 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
