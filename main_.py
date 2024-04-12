6c4565b7c6647d83ff5a71c4486f53452effd183
the-engrossed <tanmaireddy5598@gmail.com>
2024-04-12 12:09:47 -0400

    dataingestion

--- main_.py
+++ main_.py
@@ -1,3 +1,14 @@
 from winepred import logger
+from winepred.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Custom log")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
